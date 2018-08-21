b86d976ca4c36e0a33dfe2a266cf5cf0cde12a65
Kailash Chandrasekaran <mail@ckailash.com>
2018-08-21 14:00:35 +1000

    Add some extra code to the base test so the setup runs migrations only once

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,35 +2,84 @@
 
 use Faker\Factory as Faker;
 
+use Illuminate\Support\Facades\Schema;
 use Models\User;
 use Models\Account;
+use Scottlaurent\Accounting\Models\Journal;
 use Models\CompanyJournal;
 use Scottlaurent\Accounting\Models\Ledger;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class BaseTest
  */
 abstract class BaseTest extends \Orchestra\Testbench\TestCase
 {
-	
+
+	/**
+	 * @var Ledger
+	 */
+	public $company_assets_ledger;
+
+	/**
+	 * @var Ledger
+	 */
+	public $company_liability_ledger;
+
+	/**
+	 * @var Ledger
+	 */
+	public $company_equity_ledger;
+
+	/**
+	 * @var Ledger
+	 */
+	public $company_income_ledger;
+
+	/**
+	 * @var Ledger
+	 */
+	public $company_expense_ledger;
+
+	/**
+	 * @var Journal
+	 */
+	public $company_ar_journal;
+
+	/**
+	 * @var Journal
+	 */
+	public $company_cash_journal;
+
+	/**
+	 * @var Journal
+	 */
+	public $company_income_journal;
+
 	/**
      * Setup the test environment.
      */
     public function setUp()
     {
-	    
         parent::setUp();
-        
         $this->requireFilesIn(__DIR__.'/Models');
-	    
-        $this->artisan('migrate', ['--database'=>'testbench','--path'=>'migrations']);
-        $this->loadMigrationsFrom(realpath(__DIR__.'/migrations'));
-	    
+        if (!Schema::hasTable('migrations')) {
+			$hasRun = false;
+        } else {
+        	$hasRun = DB::table('migrations')->where('migration', '2017_05_21_000000_create_accounting_ledgers_table')->exists();
+        }
+
+	    if (!$hasRun) {
+		    $this->artisan('migrate', ['--database'=>'testbench','--path'=>'migrations']);
+		    $this->loadMigrationsFrom(realpath(__DIR__.'/migrations'));
+		    $this->setUpCompanyLedgersAndJournals();
+	    } else {
+		    $this->initializeLedgerAndJournals();
+	    }
+
         $this->faker = Faker::create();
-        $this->setUpCompanyLedgersAndJournals();
     }
-	
-	
+
 	/**
 	 * When using PHP Storm,
 	 * @param null $directory
@@ -59,9 +108,12 @@ abstract class BaseTest extends \Orchestra\Testbench\TestCase
 	    // Setup default database to use sqlite :memory:
 	    $app['config']->set('database.default', 'testbench');
 	    $app['config']->set('database.connections.testbench', [
-	        'driver'   => 'sqlite',
-	        'database' => ':memory:',
-	        'prefix'   => '',
+	    	'driver' => 'mysql',
+		    'host' => '192.168.10.10',
+		    'port' => '3306',
+		    'database' => 'test',
+		    'username' => 'homestead',
+		    'password' => 'secret'
 	    ]);
 	    
 	    Eloquent::unguard();
@@ -81,7 +133,7 @@ abstract class BaseTest extends \Orchestra\Testbench\TestCase
 	
 	/**
 	 * @param int $qty
-	 * @return array
+	 * @return User[]
 	 */
 	protected function createFakeUsers(int $qty) {
 		$users = [];
@@ -164,6 +216,48 @@ abstract class BaseTest extends \Orchestra\Testbench\TestCase
 		
 		$this->company_income_journal = CompanyJournal::create(['name'=>'Company Income'])->initJournal();
 		$this->company_income_journal->assignToLedger($this->company_income_ledger);
+	}
+
+	protected function initializeLedgerAndJournals() {
+		$this->company_assets_ledger = Ledger::where([
+			'name' => 'Company Assets',
+			'type' => 'asset'
+		])->first();
+
+		$this->company_liability_ledger = Ledger::where([
+			'name' => 'Company Liabilities',
+			'type' => 'liability'
+		])->first();
+
+		$this->company_equity_ledger = Ledger::where([
+			'name' => 'Company Equity',
+			'type' => 'equity'
+		])->first();
+
+		$this->company_income_ledger = Ledger::where([
+			'name' => 'Company Income',
+			'type' => 'income'
+		])->first();
+
+		$this->company_expense_ledger = Ledger::where([
+			'name' => 'Company Expenses',
+			'type' => 'expense'
+		])->first();
+
+
+		/*
+		|--------------------------------------------------------------------------
+		| This can be a bit confusing, becasue we are creating a new "company journal"
+		| Really this is just a table with a bunch of obects that we attach journals to
+		| for the company.
+		|--------------------------------------------------------------------------
+		*/
+
+		$this->company_ar_journal = CompanyJournal::where('name', 'Accounts Receivable')->first()->journal;
+
+		$this->company_cash_journal = CompanyJournal::where('name', 'Cash')->first()->journal;
+
+		$this->company_income_journal = CompanyJournal::where('name', 'Company Income')->first()->journal;
 	}
 	
 	
